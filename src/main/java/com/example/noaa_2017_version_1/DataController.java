f911ae29aa9847289170ff8ac1f2aca6cdda65e1
Ant1997 <anthonyhuynh997@gmail.com>
2021-05-16 15:31:46 -0500

    Docker Image Implemented

--- src/main/java/com/example/noaa_2017_version_1/DataController.java
+++ src/main/java/com/example/noaa_2017_version_1/DataController.java
@@ -1,27 +1,39 @@
 package com.example.noaa_2017_version_1;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 @RestController
+@Controller
 public class DataController {
     // /station?id=AEM00041194 => Lists all lines with that ID
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/station")
     public ArrayList<Data> findAllId(@RequestParam(value = "id") String idInput){
-        String file = System.getProperty("user.dir")  +
-                "\\src\\main\\java\\com\\example\\noaa_2017_version_1\\" +
-                "2017.csv";
+        String csvPath = "2017.csv";
+
         BufferedReader reader = null;
+
         String line = "";
-        ArrayList <Data> arrayList = new ArrayList<Data>();
+        ArrayList <Data> arrayList = new ArrayList<>();
         try {
-            reader = new BufferedReader(new FileReader(file));
+            System.out.println(idInput);
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream csvStream = loader.getResourceAsStream(csvPath);
+            //InputStream csvStream = this.getClass().getResourceAsStream(csvPath);
+
+            reader = new BufferedReader( new InputStreamReader(csvStream));
+
             while ((line = reader.readLine()) != null) {
 
                 String[] row = line.split(",");
